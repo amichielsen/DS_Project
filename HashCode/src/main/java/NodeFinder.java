a9870a013157db13c62d9e82df06067db8e10f1f
amichielsen <83591375+amichielsen@users.noreply.github.com>
2022-03-23 17:57:28 +0100

    NodeFinder

--- HashCode/src/main/java/NodeFinder.java
+++ HashCode/src/main/java/NodeFinder.java
@@ -1,10 +1,9 @@
-import java.util.HashMap;
-import java.util.Set;
+import java.util.*;
 
 public class NodeFinder {
 
     private Hash hashGenerator;
-    private HashMap nodeMap;
+    private HashMap<Integer, String> nodeMap;
 
     public NodeFinder(Hash hashGenerator, HashMap nodeMap) {
         this.hashGenerator = hashGenerator;
@@ -14,6 +13,20 @@ public class NodeFinder {
     public String findNodeFromFile(String filename){
         int hashCode = hashGenerator.generateHash(filename);
         Set keys = nodeMap.keySet();
-        
+        List<String> mainList = new ArrayList<String>(keys);
+        int value = usingBinarySearch(hashCode, mainList);
+        return nodeMap.get(value);
     }
-}
+
+    public static int usingBinarySearch(int value, int[] a) {
+        if (value <= a[0]) { return a[0]; }
+        if (value >= a[a.length - 1]) { return a[a.length - 1]; }
+
+        int result = Arrays.binarySearch(a, value);
+        if (result >= 0) { return a[result]; }
+
+        int insertionPoint = -result - 1;
+        return (a[insertionPoint] - value) < (value - a[insertionPoint - 1]) ?
+                a[insertionPoint] : a[insertionPoint - 1];
+
+    }
