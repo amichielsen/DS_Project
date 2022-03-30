1881e4df1a49fd4b3f6cfea57b261de2277a0937
amichielsen <83591375+amichielsen@users.noreply.github.com>
2022-03-30 17:44:10 +0200

    Broadcast

--- Node/src/main/java/com/example/node/lifeCycle/Discovery.java
+++ Node/src/main/java/com/example/node/lifeCycle/Discovery.java
@@ -1,4 +1,18 @@
 package com.example.node.lifeCycle;
 
+import java.net.*;
+
 public class Discovery extends State {
+    private static DatagramSocket socket;
+
+    public static void broadcast(String name, String IP) throws SocketException, UnknownHostException {
+        String msg = name + " " + IP;
+        socket = new DatagramSocket();
+        socket.setBroadcast(true);
+
+        byte[] buffer = msg.getBytes();
+
+        Inet4Address broadcastIP = (Inet4Address) Inet4Address.getByName("255.255.255.255");
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, broadcastIP, 8080);
+    }
 }
