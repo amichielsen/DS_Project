14a63e9859a73a1266f7ecc9f0c3c4fa4df8dcf1
amichielsen <83591375+amichielsen@users.noreply.github.com>
2022-03-30 17:51:07 +0200

    Discovery is terug

--- Node/src/main/java/com/example/node/lifeCycle/Discovery.java
+++ Node/src/main/java/com/example/node/lifeCycle/Discovery.java
@@ -2,8 +2,26 @@ package com.example.node.lifeCycle;
 
 import com.example.node.LifeCycleController;
 
+import java.io.IOException;
+import java.net.*;
+
 public class Discovery extends State {
-    public Discovery(LifeCycleController lifeCycleController) {
+    private DatagramSocket socket;
+
+    public Discovery(LifeCycleController lifeCycleController) throws SocketException {
         super(lifeCycleController);
+        socket = new DatagramSocket();
+        socket.setBroadcast(true);
     }
+
+    public void sendBroadcast(String name, String IP) throws IOException {
+        String msg = name + " " + IP;
+
+        byte[] buffer = msg.getBytes();
+        Inet4Address broadcastIP = (Inet4Address) Inet4Address.getByName("255.255.255.255");
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, broadcastIP, 8080);
+        socket.send(packet);
+        socket.close();
+    }
+
 }
