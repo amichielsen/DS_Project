db8a6d9f9a3001db8a334de3b08433bc458d59d7
amichielsen <83591375+amichielsen@users.noreply.github.com>
2022-03-30 10:59:03 +0200

    Find node of file

--- HashCode/src/main/java/NodeFinder.java
+++ HashCode/src/main/java/NodeFinder.java
@@ -5,28 +5,16 @@ public class NodeFinder {
     private Hash hashGenerator;
     private HashMap<Integer, String> nodeMap;
 
-    public NodeFinder(Hash hashGenerator, HashMap nodeMap) {
+    public NodeFinder(Hash hashGenerator, HashMap<Integer, String> nodeMap) {
         this.hashGenerator = hashGenerator;
         this.nodeMap = nodeMap;
     }
 
-    public String findNodeFromFile(String filename){
+    public int findNodeFromFile(String filename) {
         int hashCode = hashGenerator.generateHash(filename);
-        Set keys = nodeMap.keySet();
-        List<String> mainList = new ArrayList<String>(keys);
-        int value = usingBinarySearch(hashCode, mainList);
-        return nodeMap.get(value);
+        Set<Integer> nodes = nodeMap.keySet();
+        TreeSet<Integer> mainList = new TreeSet<>(nodes);
+        Integer smaller = mainList.lower(hashCode);
+        return Objects.requireNonNullElse(smaller, -1);
     }
-
-    public static int usingBinarySearch(int value, int[] a) {
-        if (value <= a[0]) { return a[0]; }
-        if (value >= a[a.length - 1]) { return a[a.length - 1]; }
-
-        int result = Arrays.binarySearch(a, value);
-        if (result >= 0) { return a[result]; }
-
-        int insertionPoint = -result - 1;
-        return (a[insertionPoint] - value) < (value - a[insertionPoint - 1]) ?
-                a[insertionPoint] : a[insertionPoint - 1];
-
-    }
+}
