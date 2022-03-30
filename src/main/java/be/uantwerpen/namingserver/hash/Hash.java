7415babec2c1e7a84072cf894b0661c1f7533541
amichielsen <83591375+amichielsen@users.noreply.github.com>
2022-03-30 16:03:58 +0200

    Hash works

--- src/main/java/be/uantwerpen/namingserver/hash/Hash.java
+++ src/main/java/be/uantwerpen/namingserver/hash/Hash.java
@@ -4,12 +4,12 @@ import static java.lang.Math.abs;
 
 public class Hash {
 
-    int max = Integer.MAX_VALUE;
-    int min = Integer.MIN_VALUE;
+    double max = Integer.MAX_VALUE;
+    double min = Integer.MIN_VALUE;
 
     public int generateHash(String hostname){
         int hashCode = hostname.hashCode();
-        int hashValue  = abs(hashCode);
+        double hashValue  = (hostname.hashCode()+max)*(32768/(max+abs(min)));
         return (int)hashValue;
     }
 }
