de1d7f5990b767d6a177c5cc23a1dbac7eb95a41
⍺ Leporis <louis.delooze@gmail.com>
2022-05-04 12:19:54 +0200

    Extra structuur

--- Node/src/main/java/be/uantwerpen/node/cache/DataLocationCache.java
+++ Node/src/main/java/be/uantwerpen/node/cache/DataLocationCache.java
@@ -1,6 +1,18 @@
 package be.uantwerpen.node.cache;
 
 
+import java.net.InetAddress;
+import java.util.HashMap;
+
 // Bevat: bestandsnaam, bastandsid, local-replica, locatie van replica
 public class DataLocationCache {
+    static DataLocationCache instance = new DataLocationCache();
+    // To be implemented...
+    // {"id": [string filename, bool R/L, integer location replica] } 
+    //private HashMap<Integer, InetAddress> cache = new HashMap<Integer, InetAddress>();
+
+    private DataLocationCache() {}
+    public static DataLocationCache getInstance(){
+        return instance;
+    }
 }
