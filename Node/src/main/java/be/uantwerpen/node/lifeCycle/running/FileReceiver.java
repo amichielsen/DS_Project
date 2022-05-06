cb974b09b242939a40a27e5e3f1bb762542d8904
amichielsen <83591375+amichielsen@users.noreply.github.com>
2022-05-06 16:44:05 +0200

    Filereceiver fix

--- Node/src/main/java/be/uantwerpen/node/lifeCycle/running/FileReceiver.java
+++ Node/src/main/java/be/uantwerpen/node/lifeCycle/running/FileReceiver.java
@@ -20,32 +20,34 @@ public class FileReceiver extends Thread{
         try{
             ServerSocket serverSocket = new ServerSocket(5044);
             System.out.println("listening to port:5044");
-            Socket clientSocket = serverSocket.accept();
-            System.out.println(clientSocket+" connected.");
-            DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
-            DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            while(true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println(clientSocket + " connected.");
+                DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
+                DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            Map responseMap = new ObjectMapper().readValue( bufferedReader.readLine(), Map.class);
-            System.out.println(responseMap);
-            String filename = (String) responseMap.get("name");
-            int size = (int) responseMap.get("length");
-            PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream());
-            printWriter.println("OK");
-            printWriter.flush();
+                Map responseMap = new ObjectMapper().readValue(bufferedReader.readLine(), Map.class);
+                System.out.println(responseMap);
+                String filename = (String) responseMap.get("name");
+                int size = (int) responseMap.get("length");
+                PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream());
+                printWriter.println("OK");
+                printWriter.flush();
 
-            FileOutputStream fileOutputStream = new FileOutputStream("/root/data/replica/" +filename);
+                FileOutputStream fileOutputStream = new FileOutputStream("/root/data/replica/" + filename);
 
-            int bytes = 0;
-            byte[] buffer = new byte[4*1024];
-            while (size > 0 && (bytes = dataInputStream.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1) {
-                fileOutputStream.write(buffer,0,bytes);
-                size -= bytes;      // read upto file size
+                int bytes = 0;
+                byte[] buffer = new byte[4 * 1024];
+                while (size > 0 && (bytes = dataInputStream.read(buffer, 0, (int) Math.min(buffer.length, size))) != -1) {
+                    fileOutputStream.write(buffer, 0, bytes);
+                    size -= bytes;      // read upto file size
+                }
             }
-            fileOutputStream.close();
-            dataInputStream.close();
-            dataOutputStream.close();
-            clientSocket.close();
+            //fileOutputStream.close();
+            //dataInputStream.close();
+            //dataOutputStream.close();
+            //clientSocket.close();
         } catch (Exception e){
             e.printStackTrace();
         }
