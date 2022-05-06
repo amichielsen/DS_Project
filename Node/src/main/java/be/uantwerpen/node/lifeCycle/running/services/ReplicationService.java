b1d5a4cef65a2f163e76fbbe4a0973b50728d304
amichielsen <83591375+amichielsen@users.noreply.github.com>
2022-05-06 16:26:08 +0200

    Replication detail

--- Node/src/main/java/be/uantwerpen/node/lifeCycle/running/services/ReplicationService.java
+++ Node/src/main/java/be/uantwerpen/node/lifeCycle/running/services/ReplicationService.java
@@ -74,8 +74,10 @@ public class ReplicationService extends Thread {
                     System.out.println(response.body());
                     JSONParser parser = new JSONParser();
                     JSONObject json = (JSONObject) parser.parse(response.body());
+                    System.out.println(response.body());
                     // Adding to ip cache
                     id = ((Long) json.get("id")).intValue();
+                    System.out.println(id);
                     ip = (String) json.get("ip");
                     IpTableCache.getInstance().addIp((Integer) json.get("id"), InetAddress.getByName((String) json.get("ip")));
 
